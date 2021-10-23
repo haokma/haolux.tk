f6829656470dd91f85897f11940fc1416074dfc2
hao2001vt <nguyenchihao2001vt@gmail.com>
2021-10-23 15:19:15 +0700

    Fix apu

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const URL_API = 'https://haoshop02.herokuapp.com'
+export const URL_API = 'https://haolux01api.herokuapp.com'
 export const URL_API_VN = 'https://thongtindoanhnghiep.co/api/city'
 export const limitProductCategory = 12
 export const limitProductNew = 16
